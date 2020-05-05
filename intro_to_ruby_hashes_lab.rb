d51219edc990af94b0571f3bf50cffbd1d4c895d
Catrina Friday <cat.friday@gmail.com>
2020-05-05 19:11:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,6 +22,7 @@ def monopoly_with_third_tier
       :rent_in_dollars=>{
         :one_piece_owned=>25,
         :two_pieces_owned=>50,
+        :three_pieces_owned=>100,
         :four_pieces_owned=>200
       },
       :names=>{
@@ -35,8 +36,6 @@ def monopoly_with_third_tier
 end
   
   
-
-
 def monopoly_with_fourth_tier
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
